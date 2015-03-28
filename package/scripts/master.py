dbf5e762ae46c8740c312cc577e06f3d695a008e
Ali Bajwa <abajwa@hortonworks.com>
2015-03-27 18:56:24 -0700

    added comments

--- package/scripts/master.py
+++ package/scripts/master.py
@@ -8,20 +8,22 @@ class Master(Script):
     # Install packages listed in metainfo.xml
     self.install_packages(env)
     
-    #Run any other install steps needed
+    #if any other install steps were needed they can be added here
 
+  #To stop the service, use the linux service stop command and pipe output to log file
   def stop(self, env):
-    import params
+    import params 
     Execute('service ntpd stop >>' + params.stack_log)
-      
+
+  #To start the service, use the linux service start command and pipe output to log file      
   def start(self, env):
     import params
     Execute('service ntpd start >>' + params.stack_log)
 	
-
+  #To get status of the, use the linux service status command      
   def status(self, env):
     import params
-    Execute('service ntpd status >>' + params.stack_log)
+    Execute('service ntpd status')
 
 if __name__ == "__main__":
   Master().execute()
--- package/scripts/params.py
+++ package/scripts/params.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 from resource_management import *
 
-# server configurations
+# config object that holds the configurations declared in the config xml file
 config = Script.get_config()
 
+# store the log file for the service from the 'ntpd.log' property of the 'ntpd-config.xml' file
 stack_log = config['configurations']['ntpd-config']['ntpd.log']
 
