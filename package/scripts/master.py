7eeca7a54c08d823fc8561da12822b718d91b568
abajwa-hw <ali.bajwa@gmail.com>
2018-04-03 03:08:21 +0000

    updated for Centos 7

--- package/scripts/master.py
+++ package/scripts/master.py
@@ -5,6 +5,7 @@ class Master(Script):
     def install(self, env):
         # Install packages listed in metainfo.xml
         self.install_packages(env)
+        Execute('echo \'OPTIONS="-g -p /var/run/ntpd.pid"\' > /etc/sysconfig/ntpd')
         #if any other install steps were needed they can be added here
 
     #To stop the service, use the linux service stop command and pipe output to log file
@@ -12,6 +13,7 @@ class Master(Script):
         import params
         env.set_params(params)
         Execute('service ntpd stop >>' + params.stack_log)
+        Execute('rm -f /var/run/ntpd.pid')
 
     #To start the service, use the linux service start command and pipe output to log file
     def start(self, env):
@@ -21,8 +23,8 @@ class Master(Script):
 
     #To get status of the, use the linux service status command
     def status(self, env):
-        import params
-        Execute('service ntpd status')
+        
+        check_process_status('/var/run/ntpd.pid')
 
 if __name__ == "__main__":
     Master().execute()
